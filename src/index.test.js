2d35b12cc8109991cbcabee7021bd3b3503b3af4
Don Johnson <copyleftdev@gmail.com>
2016-01-01 08:04:39 -0800

    Added tests

---
+++ src/index.test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+var starWars = require('./index');
+
+
+describe('starwars-names', function() {
+  it ('should work!', function() {
+    expect(true).to.be.true;
+  });
+});
